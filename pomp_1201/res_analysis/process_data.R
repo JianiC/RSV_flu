a527be57bda4cb1ae3f1e916464a13f512061dee
Jiani Chen <chenjiani930522@gmail.com>
2021-12-17 11:30:20 -0500

    update

--- pomp_1201/res_analysis/process_data.R
+++ pomp_1201/res_analysis/process_data.R
@@ -1,11 +1,11 @@
 source("./setup.R")
 source("./util_funs.R")
-
+library(lubridate)
 # load the data csv
 
-data <- read.csv("./pomp_HHSregion_case.csv")
+data <- read.csv("../original_data/pomp_HHSregion_case.csv")
 
-demog <- read.csv("./pop_HHSRegion.csv") %.>% 
+demog <- read.csv("../original_data/pop_HHSRegion.csv") %.>% 
   mutate(., HHS_REGION = HHS_region) %.>%
   select(., 4,3) 
 
@@ -30,9 +30,110 @@ inc_data <- (
   )
 
 # save(inc_data, file = "inc_data.rds")
+time_add<-c(decimal_date(ymd("2014-01-04")),decimal_date(ymd("2015-01-03")),decimal_date(ymd("2016-01-02")))
+
+## add missing date point to inc data
 
 
 
+# save(inc_data, file = "inc_data.rds")
+
+time_add<-c(decimal_date(ymd("2014-01-04")),decimal_date(ymd("2015-01-03")),
+            decimal_date(ymd("2016-01-02")),decimal_date(ymd("2018-01-06"))
+                         )
+
+inc_data_add <- (
+  data %.>% 
+    rbind(., 
+          tibble(date = data$date %.>% min(.) - 1/52,
+                 time = data$time %.>% min(.) - 1/52, 
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10)) %.>%
+    rbind(.,
+          tibble(date=time_add[1],
+                 time=time_add[1]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    rbind(.,
+          tibble(date=time_add[2],
+                 time=time_add[2]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    rbind(.,
+          tibble(date=time_add[3],
+                 time=time_add[3]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    right_join(., demog, by = "HHS_REGION") %.>% 
+    arrange(., time) %.>% 
+    gather(., key = "virus", value = "cases", -c(time, date, HHS_REGION, pop_ave_sum)) %.>% 
+    mutate(.,
+           N = pop_ave_sum,
+           virus = str_remove(virus, "pos")) %.>% 
+    select(., -pop_ave_sum)
+)
+
+
+#save(inc_data_add, file = "inc_data_add.rds")
+
+data_perdic <- read.csv("../original_data/pomp_HHSregion_case_2018.csv")
+
+inc_data_perdic <- (
+  data_perdic %.>% 
+    rbind(., 
+          tibble(date = data$date %.>% min(.) - 1/52,
+                 time = data$time %.>% min(.) - 1/52, 
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10)) %.>%
+    rbind(.,
+          tibble(date=time_add[1],
+                 time=time_add[1]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    rbind(.,
+          tibble(date=time_add[2],
+                 time=time_add[2]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    rbind(.,
+          tibble(date=time_add[3],
+                 time=time_add[3]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    rbind(.,
+          tibble(date=time_add[4],
+                 time=time_add[4]-2011,
+                 RSVpos =  NA, 
+                 fluApos = NA, 
+                 fluBpos = NA, 
+                 HHS_REGION = 1:10))%.>%
+    right_join(., demog, by = "HHS_REGION") %.>% 
+    arrange(., time) %.>% 
+    gather(., key = "virus", value = "cases", -c(time, date, HHS_REGION, pop_ave_sum)) %.>% 
+    mutate(.,
+           N = pop_ave_sum,
+           virus = str_remove(virus, "pos")) %.>% 
+    select(., -pop_ave_sum)
+)
+
+
+save(inc_data_perdic, file = "inc_data_perdic.rds")
 
 
   
