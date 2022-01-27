f1cfd2b8b95096348ee9cc7f7ee55963f921eb72
gokhale <39336684+gokhale616@users.noreply.github.com>
2022-01-27 11:10:31 -0500

    Added comments for estimation protocol
    Check with you in a bit!

--- pomp_1201/clsuter_pomp/loglik_profile_0127/setup.R
+++ pomp_1201/clsuter_pomp/loglik_profile_0127/setup.R
@@ -15,6 +15,29 @@ load("./inc_data_add.rds")
 
 # set up controls for deoptim
 ### controls list for DEoptim - for MLE estimation
+# for the first try - range 0, 1 on chi
+# steptol = 200
+# np_val = 150
+
+# if the parameter is poorly identified i.e. the likelihood surface is flat
+# then {
+# for the second try - range 0.75*mle, 1.25*mle on chi
+# steptol = 200
+# np_val = 150
+# } else {
+
+#for the second try - range 0.75*mle, 1.25*mle on chi
+# increase the strictness of the optimization problem
+# steptol = 500
+# np_val = 500
+# }
+
+# finally check if the increasing further values makes any difference 
+# basically 
+# steptol = 750
+# np_val = 750
+
+
 np_val = 750
 my_controls <- list(itermax = 1e6,
                     F = 0.6, CR = 0.9, 
